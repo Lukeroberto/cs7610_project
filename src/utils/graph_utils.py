1719cf52ef9d84736fef8f62eb60915b4334a44c
luke <lroberto@ccs.neu.edu>
2019-11-21 11:12:46 -0500

    added 3 new network topologies

--- src/utils/graph_utils.py
+++ src/utils/graph_utils.py
@@ -27,3 +27,30 @@ def chain_adj(length):
 
     return adj
 
+def full_adj(length):
+    adj = np.ones((length, length), dtype=np.int)
+    diag = np.diag_indices(length)
+    adj[diag] = 0
+
+    return adj
+
+def spoke_adj(length):
+    adj = np.zeros((length, length), dtype=np.int)
+
+    adj[0, 1:] = 1
+    adj[1:, 0] = 1
+
+    return adj
+
+def network_partition():
+    # 2 triangles connected
+    adj = np.zeros((6,6), dtype=np.int)
+
+    adj[0, [1, 2]] = 1
+    adj[1, [0, 2]] = 1
+    adj[2, [0, 1, 3]] = 1
+    adj[3, [2, 4, 5]] = 1
+    adj[4, [3, 5]] = 1
+    adj[5, [3, 4]] = 1
+
+    return adj
