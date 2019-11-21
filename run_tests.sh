f327dbdbca06056b97df46eb646f9d2b909ef803
luke <lroberto@ccs.neu.edu>
2019-11-21 11:17:42 -0500

    Added skeleton of testing file

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run several test
+
+# Test 1: Fully Connected
+
+# Test 2: Network Partitions (2 Types)
+# Test 2a: Loose partition
+
+# Test 2b: Extreme partition
+
+# Test 3: Chain
+# Test 3a: Vanilla Chain
+
+# Test 3b: Chain with only 1 agent/ no learning anywhere
